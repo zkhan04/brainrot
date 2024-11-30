ddfed94ea2f4b2bcc4f32675e29b1b7cdbfdc700
mi55a <milymendez678@gmail.com>
2024-11-29 21:58:11 -0600

    mulch dog appears for a bit. mulch gang for life!

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const body = document.querySelector("body");
 
 body.style.cursor = `url(${chrome.runtime.getURL('assets/mango.png')}),auto`
 
+const mulchImage = chrome.runtime.getURL('assets/mulch2.jpeg');
 const hawkTuahAudio = new Audio(chrome.runtime.getURL('assets/hawktuahTrim.mp3'));
 const mangoAudio = new Audio(chrome.runtime.getURL('assets/mangos.mp3'));
+const mulchAudio = new Audio(chrome.runtime.getURL('assets/mulch.mp3'));
 const buttons = document.querySelectorAll("button")
 
 buttons.forEach(button => {
@@ -25,7 +27,33 @@ buttons.forEach(button => {
         });
         console.log("Mangooooo");
     });
+
+    button.addEventListener('mouseout', () => {
+        mulchImageRandom();
+    })
 });
 
+function mulchImageRandom() {
+    const image = document.createElement('img');
+    image.src = mulchImage;
+
+    mulchAudio.currentTime = 0;
+    mulchAudio.play().catch((err) => {
+        console.error("Error with audio: ", err);
+    });
+    console.log("Mulch gang for life!!!");
+    console.log("Mulch image URL: ", mulchImage);
+
+    image.style.position = 'fixed';
+    image.style.top = '100px';
+    image.style.left = '100px';
+    image.style.zIndex = '9999';
+
+    document.body.appendChild(image);
+
+    setTimeout(()=> {
+        image.remove();
+    }, 5000);
+};
 
 
