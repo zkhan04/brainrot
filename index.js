76e7ec8b5876bfc80405ced8035f902da95318c6
mi55a <milymendez678@gmail.com>
2024-11-29 21:10:06 -0600

    hover button and click button events

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const body = document.querySelector("body");
 body.style.cursor = `url(${chrome.runtime.getURL('assets/mango.png')}),auto`
 
 const hawkTuahAudio = new Audio(chrome.runtime.getURL('assets/hawktuahTrim.mp3'));
+const mangoAudio = new Audio(chrome.runtime.getURL('assets/mangos.mp3'));
 const buttons = document.querySelectorAll("button")
 
 buttons.forEach(button => {
+    
     button.addEventListener("mouseover", () => {
         // Hawk tuah spit on that thing :3
         hawkTuahAudio.currentTime = 0;
@@ -15,5 +17,16 @@ buttons.forEach(button => {
         // debug
         console.log("Spit on that thing");
     });
+    
+    button.addEventListener('click', () => {
+        mangoAudio.currentTime = 6;
+        mangoAudio.play().catch((err) => {
+            console.error("Error with audio: ", err);
+        });
+        console.log("I understand it now");
+        console.log(chrome.runtime.getURL('assets/mangos.mp3'));
+    });
 });
+
+
 
