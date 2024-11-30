a756655445796fa57f78e221eb196347352817f1
mi55a <milymendez678@gmail.com>
2024-11-29 18:20:11 -0600

    hawk tuah hover should be working now

--- index.js
+++ index.js
@@ -2,13 +2,18 @@ const body = document.querySelector("body");
 
 body.style.cursor = `url(${chrome.runtime.getURL('assets/mango.png')}),auto`
 
-const button = document.querySelector("button")
 const hawkTuahAudio = new Audio(chrome.runtime.getURL('assets/hawktuahTrim.mp3'));
+const buttons = document.querySelectorAll("button")
 
-button.addEventListener("mouseover", () => {
-    // Hawk tuah spit on that thing :3
-    hawkTuahAudio.currentTime = 0;
-    hawkTuahAudio.play();
-    // debug
-    console.log("Spit on that thing");
+buttons.forEach(button => {
+    button.addEventListener("mouseover", () => {
+        // Hawk tuah spit on that thing :3
+        hawkTuahAudio.currentTime = 0;
+        hawkTuahAudio.play().catch((err) => {
+            console.error("Error with audio: ", err);
+        });
+        // debug
+        console.log("Spit on that thing");
+    });
 });
+
