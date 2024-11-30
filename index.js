d54d4db7be5da9725ea4716cd6c12f38126fa9d2
mi55a <milymendez678@gmail.com>
2024-11-29 18:00:06 -0600

    hover over button, hawk tuah (wip)

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 const body = document.querySelector("body");
 
 body.style.cursor = `url(${chrome.runtime.getURL('assets/mango.png')}),auto`
+
+const button = document.querySelector("button")
+const hawkTuahAudio = new Audio(chrome.runtime.getURL('assets/hawktuahTrim.mp3'));
+
+button.addEventListener("mouseover", () => {
+    // Hawk tuah spit on that thing :3
+    hawkTuahAudio.currentTime = 0;
+    hawkTuahAudio.play();
+    // debug
+    console.log("Spit on that thing");
+});
