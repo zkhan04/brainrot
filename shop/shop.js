8a40ced8acc1cc0f26d8f461ca02608ac81cf3f6
mi55a <milymendez678@gmail.com>
2024-12-01 17:38:42 -0600

    brainrot shop (wip)

--- shop/shop.js
+++ shop/shop.js
@@ -1,6 +1,10 @@
 const body = document.querySelector("body");
 const pointsDiv = document.createElement("div");
 pointsDiv.innerHTML = "pointsDiv"
+/*
+pointsDiv.className = 'points-div';
+pointsDiv.classList.add('pink-bg');
+*/
 body.appendChild(pointsDiv);
 
 document.addEventListener("DOMContentLoaded", async () => {
